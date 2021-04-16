00c90ac57960dde24ad397ffccf7ea0423c84cf2
Akshat Agrawal <adroit@Akshats-MacBook-Air.local>
2021-04-16 13:21:29 +0530

    refactored test cases as added an init block

--- src/test/java/org/tictactoe/tdd/demo/TicTacToeTest.java
+++ src/test/java/org/tictactoe/tdd/demo/TicTacToeTest.java
@@ -2,27 +2,32 @@ package org.tictactoe.tdd.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TicTacToeTest {
 
+	private TicTacToe game;
+	
+	@BeforeEach
+	public void init() {
+		game = new TicTacToe();
+	}
+	
 	@Test
 	void checkForNoResultIfNewGame() {
-		TicTacToe game = new TicTacToe();
 		String winningTeam = game.calculateResult(' ');
 		assertEquals(null, winningTeam);
 	}
 	
 	@Test
 	void checkIfPlayerOHasMadeAMove() {
-		TicTacToe game = new TicTacToe();
 		String winningTeam = game.calculateResult('O');
 		assertEquals("PlayerO", winningTeam);
 	}
 	
 	@Test
 	void checkIfPlayerXHasMadeAMove() {
-		TicTacToe game = new TicTacToe();
 		String winningTeam = game.calculateResult('X');
 		assertEquals("PlayerX", winningTeam);
 	}
